c0446c9ca57c714dbd892b2a3369f3409a7fc8db
bence-balazs <bestbence@gmail.com>
2025-09-23 17:13:20 +0200

    test

--- ubuntu.sh
+++ ubuntu.sh
@@ -202,9 +202,9 @@ levels:
 - [7, 65, 74]
 - [127, 70, 32767]
 EOF
-    echo "options thinkpad_acpi fan_control=1" | sudo tee /etc/modprobe.d/thinkfan.conf
-    sudo modprobe -r thinkpad_acpi
-    sudo modprobe thinkpad_acpi
+    # echo "options thinkpad_acpi fan_control=1" | sudo tee /etc/modprobe.d/thinkfan.conf
+    # sudo modprobe -r thinkpad_acpi
+    # sudo modprobe thinkpad_acpi
     sudo systemctl enable thinkfan
 }
 
